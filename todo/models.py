5bb6c0eeaa50db59143a8adff1a9648308e1fbb6
Raghav <70934347+raggzy271@users.noreply.github.com>
2025-02-23 01:15:42 +0530

    Add manual primary keys in models

--- todo/models.py
+++ todo/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+import uuid
 
 class LastSaved(models.Model):
     timestamp = models.DateTimeField(auto_now=True)
 
 class TodoList(models.Model):
+    id = models.UUIDField(primary_key=True, editable=True)
     name = models.CharField(max_length=255)
 
 class TodoCard(models.Model):
+    id = models.UUIDField(primary_key=True, editable=True)
     text = models.TextField()
     listId = models.ForeignKey(TodoList, on_delete=models.CASCADE, related_name="cards")
     completed = models.BooleanField(default=False)
