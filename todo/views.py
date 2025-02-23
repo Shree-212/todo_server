9016caca50372b13e97753ebc9ee65d9c802726c
Raghav <70934347+raggzy271@users.noreply.github.com>
2025-02-23 20:23:54 +0530

    Add sync function

--- todo/views.py
+++ todo/views.py
@@ -3,7 +3,8 @@ from rest_framework import viewsets
 from .models import TodoList, TodoCard, LastSaved
 from .serializers import ToDoListSerializer, ToDoCardSerializer, LastSavedSerializer
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK
+from rest_framework.decorators import action
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 
 def update_last_saved():
     """Updates the last saved timestamp in the database."""
@@ -26,6 +27,41 @@ class LastSavedMixin:
 class ToDoListViewSet(LastSavedMixin, viewsets.ModelViewSet):
     queryset = TodoList.objects.all()
     serializer_class = ToDoListSerializer
+
+    @action(detail=False, methods=["post"])
+    def sync(self, request):
+        """Clear all to-do lists and cards, then add the new ones."""
+        # Clear the TodoList and TodoCard tables
+        TodoList.objects.all().delete()
+        TodoCard.objects.all().delete()
+
+        # Get new TodoLists and TodoCards from request data
+        todo_lists_data = request.data.get("lists", [])
+        todo_cards_data = request.data.get("cards", [])
+        
+        try:
+            # Bulk create new TodoLists
+            todo_lists = [TodoList(**data) for data in todo_lists_data]
+            TodoList.objects.bulk_create(todo_lists)
+
+            # Now that TodoLists are created, we need to assign the correct TodoList instance to TodoCard's foreign key
+            for card_data in todo_cards_data:
+                # Fetch the corresponding TodoList instance by ID
+                todo_list_instance = TodoList.objects.get(id=card_data["listId"])
+
+                # Assign the TodoList instance to the card's foreign key
+                card_data["listId"] = todo_list_instance
+
+            # Bulk create new TodoCards with correct foreign key relationship
+            todo_cards = [TodoCard(**data) for data in todo_cards_data]
+            TodoCard.objects.bulk_create(todo_cards)
+
+            # Update the last saved timestamp
+            update_last_saved()
+
+            return Response({"success": True}, status=HTTP_200_OK)
+        except Exception as e:
+            return Response({"error": str(e)}, status=HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
